63c9a905bb46f430e8fc8e82c257e8c6d96456d9
Cet500 <codeoon@mail.ru>
2023-08-29 12:10:36 +1000

    Full change from staic to dynamic
    All site changed to dynamic.
    All pages reworked to jinja2-templates.
    All additional content moved to static.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,49 @@
+from flask import render_template
+from flask_babel import _, get_locale
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template("index.html")
+
+
+@app.route('/catalog')
+def catalog():
+    return render_template("catalog.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
+
+
+@app.route('/price_list')
+def price_list():
+    return render_template("price_list.html")
+
+
+@app.route('/info_bear')
+def info_bear():
+    return render_template("info_bear.html")
+
+
+@app.route('/info_penguin')
+def info_penguin():
+    return render_template("info_penguin.html")
+
+
+@app.route('/info_puppy')
+def info_puppy():
+    return render_template("info_puppy.html")
